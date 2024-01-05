a15294a739834a399e73a444c67ee012aa8dfc7f
Igor Babko <i.i.babko@gmail.com>
2024-01-05 13:45:46 +0200

    7 - constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,19 @@
-import { ICON_CLOCK, ICON_LIST_BULLET, ICON_CHART_BAR } from './icons'
 import { generatePeriodSelectOptions } from './functions'
+import { ICON_CHART_BAR, ICON_CLOCK, ICON_LIST_BULLET } from './icons'
+
+type PageName = typeof PAGE_TIMELINE | typeof PAGE_ACTIVITIES | typeof PAGE_PROGRESS
+
+type ButtonType =
+  | typeof BUTTON_TYPE_PRIMARY
+  | typeof BUTTON_TYPE_SUCCESS
+  | typeof BUTTON_TYPE_WARNING
+  | typeof BUTTON_TYPE_NEUTRAL
+  | typeof BUTTON_TYPE_DANGER
+
+interface NavItem {
+  page: PageName
+  icon: any
+}
 
 export const LOCAL_STORAGE_KEY = 'time-tracker'
 
@@ -7,7 +21,7 @@ export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 
-export const NAV_ITEMS = [
+export const NAV_ITEMS: NavItem[] = [
   {
     page: PAGE_TIMELINE,
     icon: ICON_CLOCK
@@ -28,7 +42,7 @@ export const BUTTON_TYPE_WARNING = 'warning'
 export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_DANGER = 'danger'
 
-export const BUTTON_TYPES = [
+export const BUTTON_TYPES: ButtonType[] = [
   BUTTON_TYPE_PRIMARY,
   BUTTON_TYPE_SUCCESS,
   BUTTON_TYPE_WARNING,
@@ -44,7 +58,7 @@ export const MIDNIGHT_HOUR = 0
 export const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
 export const SECONDS_IN_DAY = HOURS_IN_DAY * SECONDS_IN_HOUR
 
-export const PERIOD_SELECT_OPTIONS = generatePeriodSelectOptions()
+export const PERIOD_SELECT_OPTIONS: any = generatePeriodSelectOptions()
 
 export const LOW_PERCENT = 33
 export const MEDIUM_PERCENT = 66
