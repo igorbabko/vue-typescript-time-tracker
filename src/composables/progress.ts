842e0a8a2cc4ca51c8ef84b200a6523f52f78813
Igor Babko <i.i.babko@gmail.com>
2024-01-18 10:52:00 +0200

    18. Typing composables

---
+++ src/composables/progress.ts
@@ -0,0 +1,27 @@
+import { computed, type ComputedRef } from 'vue'
+import { calculateActivityCompletionPercentage } from '../activities'
+import { getProgressColorClass } from '../functions'
+import { calculateTrackedActivitySeconds, timelineItems } from '../timeline-items'
+import type { Activity, ProgressColorClass } from '../types'
+
+export function useProgress(activity: Activity): {
+  colorClass: ComputedRef<ProgressColorClass>
+  percentage: ComputedRef<number>
+  trackedActivitySeconds: ComputedRef<number>
+} {
+  const colorClass = computed((): ProgressColorClass => getProgressColorClass(percentage.value))
+
+  const percentage = computed((): number =>
+    calculateActivityCompletionPercentage(activity, trackedActivitySeconds.value)
+  )
+
+  const trackedActivitySeconds = computed((): number =>
+    calculateTrackedActivitySeconds(timelineItems.value, activity)
+  )
+
+  return {
+    colorClass,
+    percentage,
+    trackedActivitySeconds
+  }
+}
---
+++ src/composables/total-progress.ts
@@ -0,0 +1,32 @@
+import { computed, type ComputedRef } from 'vue'
+import { calculateCompletionPercentage, trackedActivities } from '../activities'
+import { getProgressColorClass } from '../functions'
+import { calculateTrackedActivitySeconds, timelineItems } from '../timeline-items'
+import type { ProgressColorClass } from '../types'
+
+export function useTotalProgress(): {
+  colorClass: ComputedRef<ProgressColorClass>
+  percentage: ComputedRef<number>
+} {
+  const colorClass = computed((): ProgressColorClass => getProgressColorClass(percentage.value))
+
+  const percentage = computed((): number =>
+    calculateCompletionPercentage(totalTrackedSeconds.value)
+  )
+
+  const totalTrackedSeconds = computed((): number => {
+    return trackedActivities.value
+      .map((activity): number =>
+        Math.min(
+          calculateTrackedActivitySeconds(timelineItems.value, activity),
+          activity.secondsToComplete
+        )
+      )
+      .reduce((total, seconds): number => total + seconds, 0)
+  })
+
+  return {
+    colorClass,
+    percentage
+  }
+}
