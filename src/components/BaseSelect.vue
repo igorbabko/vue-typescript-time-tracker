5d15f29a72b936ee409e4f33dca86ce17e451aee
Igor Babko <i.i.babko@gmail.com>
2024-01-20 12:36:07 +0200

    22. Generic BaseSelect component

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T extends number | string">
 import { computed } from 'vue'
 import { BUTTON_TYPE_NEUTRAL } from '../constants'
 import { normalizeSelectValue } from '../functions'
@@ -6,15 +6,16 @@ import { ICON_X_MARK } from '../icons'
 import { isUndefinedOrNull } from '../validators'
 import BaseButton from './BaseButton.vue'
 import BaseIcon from './BaseIcon.vue'
+import type { SelectOption } from '../types'
 
 const props = defineProps<{
-  options: { value: number | string; label: string }[]
-  selected: number | string | null
+  options: SelectOption<T>[]
+  selected: T | null
   placeholder: string
 }>()
 
 const emit = defineEmits<{
-  select: [value: number | string | null]
+  select: [value: T | null]
 }>()
 
 const isNotSelected = computed((): boolean => isUndefinedOrNull(props.selected))
